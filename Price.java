e0888ac6eb69aa3e79587621f0aa7b2677015c3d
caioguedesam <caioguedesam@gmail.com>
2020-10-14 16:13:22 -0300

    Commit 16

--- Price.java
+++ Price.java
@@ -1,7 +1,7 @@
 public abstract class Price {
-   public abstract int getPriceCode();
-   public abstract double getCharge(int daysRented);
-   public int getFrequentRenterPoints(int daysRented) {
-     return 1;
-   }
- }
+  public abstract int getPriceCode();
+  public abstract double getCharge(int daysRented);
+  public int getFrequentRenterPoints(int daysRented) {
+    return 1;
+  }
+}

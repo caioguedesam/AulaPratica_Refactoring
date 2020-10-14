e54107601a43d3e64fa4077dcb5ae039d1e70c59
caioguedesam <caioguedesam@gmail.com>
2020-10-14 15:38:33 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -35,5 +35,12 @@ public class Rental {
                break;
         }
       return thisAmount;
-   }
+  }
+
+  public int getFrequentRenterPoints() {
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+      return 2;
+    else 
+      return 1;
+  }
 }
